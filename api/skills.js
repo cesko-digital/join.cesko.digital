0c92392ecca9a0acab03f7950545a36d0c0a5110
Pavel Suchmann <pavel.suchmann@gmail.com>
2021-01-16 15:49:36 +0100

    skills paging implemented

--- api/skills.js
+++ api/skills.js
@@ -1,6 +1,6 @@
 
 import axios from 'axios'
-//import querystring from 'querystring'
+import querystring from 'querystring'
 
 const uri_part = process.env.AIRTABLE_URI_PART
 const api_key = process.env.AIRTABLE_API_KEY
@@ -13,24 +13,51 @@ const http = axios.create({
   }
 })
 
-module.exports = (req, res) => {
-  http.get('/Skills')
-  .then(response => {
-    console.log(response.data)
-    const skills = {}
-    for (const record of response.data.records) {
-      const key = record.fields.Field
-      if (key in skills) {
-        skills[key].push({
-          id: record.id,
-          text: record.fields.Subfield
-        })
-      } else {
-        skills[key] = [{
-          id: record.id,
-          text: record.fields.Subfield
-        }]
+const getPage = (skills, offset=null) => {
+  return new Promise((resolve, reject) => {
+    const query = {
+      pageSize: 100
+    }
+    if (offset) {
+      query.offset = offset
+    }
+    http.get('/Skills?' + querystring.stringify(query))
+    .then(response => {
+      for (const record of response.data.records) {
+        const key = record.fields.Field
+        if (key in skills) {
+          skills[key].push({
+            id: record.id,
+            text: record.fields.Subfield
+          })
+        } else {
+          skills[key] = [{
+            id: record.id,
+            text: record.fields.Subfield
+          }]
+        }
       }
+      resolve({
+        skills: skills,
+        offset: response.data.offset
+      })
+    })
+    .catch(e => {
+      console.log(e)
+      reject({
+        message: JSON.stringify(e)
+      })
+    })
+  })
+}
+
+module.exports = async (req, res) => {
+  try {
+    let skills = {}
+    let page = await getPage(skills)
+    while (page.offset) {
+      skills = page.skills
+      page = await getPage(skills, page.offset)
     }
     const skills_array = []
     for (const field in skills) {
@@ -51,15 +78,11 @@ module.exports = (req, res) => {
       item['details'] = details
       skills_array.push(item)
     }
-    res.status(200).json({
-      skills: skills_array
-    })
-  })
-  .catch(e => {
-    console.log(e)
+    res.status(200).json({ skills: skills_array })
+  } catch (err) {
     res.status(500).json({
       code: 50000,
-      message: JSON.stringify(e)
+      message: JSON.stringify(err)
     })
-  })
+  }
 }
