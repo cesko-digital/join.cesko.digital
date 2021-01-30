13fc1065ffe538b5434079baff653c77be658982
Jakub Krupka <jkrupka@twilio.com>
2021-01-30 20:08:08 +0100

    add callout component

---
+++ src/components/callout/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import * as S from './styles'
+
+export interface Props {
+  type: 'success' | 'error'
+}
+
+const Callout: React.FC<Props> = ({ children, ...props }) => {
+  return (
+    <S.CalloutBox type={props.type} role="alert">
+      {children}
+    </S.CalloutBox>
+  )
+}
+
+export default Callout
