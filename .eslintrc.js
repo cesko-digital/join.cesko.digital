91bf7303a2dba2ff49cc48606d8674a153ca38b5
Jakub Krupka <jkrupka@twilio.com>
2021-02-05 14:26:49 +0100

    lint and fix code style for api files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,33 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    extends: [
-      "plugin:@typescript-eslint/recommended",
-      "prettier/@typescript-eslint",
-      "plugin:prettier/recommended",
-      "plugin:react/recommended",
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/prop-types': ['off', { ignore: ['children'] }],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    'no-warning-comments': [
+      'error',
+      { terms: ['todo', 'fixme'], location: 'anywhere' },
     ],
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: "module",
-    },
-    rules: {
-      "no-console": "error",
-      "@typescript-eslint/no-explicit-any": "error",
-      "@typescript-eslint/no-unused-vars": "error",
-      "@typescript-eslint/explicit-module-boundary-types": "off",
-      "react/prop-types": ["off", { ignore: ["children"] }],
-      "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
-      "no-warning-comments": [
-        "error",
-        { terms: ["todo", "fixme"], location: "anywhere" },
-      ]
-    },
-    overrides: [
-      {
-        files: ["gatsby-config.js"],
-        rules: {
-          "@typescript-eslint/no-var-requires": "off",
-        },
+  },
+  overrides: [
+    {
+      files: ['gatsby-config.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
       },
-    ],
-  };
-  
+    },
+  ],
+}
